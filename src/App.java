c6a757248ee0ab9aba0e34675e2b5cb4040ab997
rafaaxits <rafaaxits@gmail.com>
2023-07-24 18:38:18 -0300

    adicionando um metodo soma no main do projeto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Ediane!");
+        int resultado = soma(10, 15);
+        System.out.println("Resultado: " + resultado);
+    }
+
+    public static int soma(int x, int y) {
+        return x+y;
     }
 }
